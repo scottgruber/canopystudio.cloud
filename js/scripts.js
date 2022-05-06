a54d9bfcdc205baa4e9dabd494111c060665ac48
Scott Gruber <scott@scottgruber.me>
2022-05-05 17:09:50 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '627462fb26d6e2393b983db6');
+  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+  t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
+
